fe504dfacccc1de18cb686b30ee8ad42e1f055c3
julianmedwards <julian.michael.edwards@gmail.com>
2024-04-05 13:07:07 -0500

    Broken; moving to other machine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Widget from './widget/Widget';
+import { Widget as TestWidget } from '@acm-widgets/testwidget';
 import './App.css';
 
 // DO NOT CHANGE
@@ -6,7 +6,9 @@ function App() {
     return (
         <>
             <h1 className="main-title">Austin Code Mentorship Widgets</h1>
-            <div className="widgets"></div>
+            <div className="widgets">
+                <TestWidget />
+            </div>
             <a
                 href="https://github.com/me-julian/acm-widgets"
                 target="_blank"
