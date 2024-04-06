2de64a8119632025d5cbb11cf78d5ef6c76c45ce
julianmedwards <julian.michael.edwards@gmail.com>
2024-04-06 01:44:10 -0500

    Add example second test widget.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import TestTwoWidget from '@acm-widgets/testtwo/frontend/src/widget/Widget.jsx';
+import TestThreeWidget from '@acm-widgets/testthree/frontend/src/widget/Widget.jsx';
+
 import './App.css';
 
 // DO NOT CHANGE
@@ -8,6 +10,9 @@ function App() {
             <h1 className="main-title">Austin Code Mentorship Widgets</h1>
             <div className="widgets">
                 <TestTwoWidget apiUrl={`http://127.0.0.1:5000/api/testtwo`} />
+                <TestThreeWidget
+                    apiUrl={`http://127.0.0.1:5000/api/testthree`}
+                />
             </div>
             <a
                 href="https://github.com/me-julian/acm-widgets"
