c7b0e894351335d5b671286952d1079c700b1f43
julianmedwards <julian.michael.edwards@gmail.com>
2024-04-06 01:32:03 -0500

    Remove databases, extra test as example.

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import db from './db.js';
 
 const app = express();
 
--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import testtwoRouter from '@acm-widgets/testtwo/backend/router.js';
+import testthreeRouter from '@acm-widgets/testthree/backend/router.js';
 
 const router = Router();
 router.use('/testtwo', testtwoRouter);
+router.use('/testthree', testthreeRouter);
 
 export default router;
