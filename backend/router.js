554f646b2f28c33ce05d1c1fb2d50b622ccbe28e
julianmedwards <julian.michael.edwards@gmail.com>
2024-04-05 23:43:02 -0500

    Update styling; remove test widget pkg

--- backend/router.js
+++ backend/router.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import testtwoRouter from '@acm-widgets/testtwo/backend/router.js';
-import kevintestRouter from '@acm-widgets/kevintest/backend/router.js';
 
 const router = Router();
 router.use('/testtwo', testtwoRouter);
-router.use('/kevintest', kevintestRouter);
 
 export default router;
